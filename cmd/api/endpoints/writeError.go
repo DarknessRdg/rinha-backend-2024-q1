d28c938dd81ff8c79a20f2ff6c590fcf47354a77
Luan Rodrigues <luanrodrigues007@hotmail.com>
2024-02-18 23:24:28 -0300

    feat: improve error json detail

--- cmd/api/endpoints/writeError.go
+++ cmd/api/endpoints/writeError.go
@@ -1,6 +1,8 @@
 package endpoints
 
 import (
+	"encoding/json"
+	"log/slog"
 	"net/http"
 
 	"github.com/DarknessRdg/rinha-backend-2024-q1/internal/errs"
@@ -11,7 +13,20 @@ func writeError(err error, response http.ResponseWriter) {
 
 	if ok {
 		response.WriteHeader(int(apiErr.StatusCode))
+	} else {
+		response.WriteHeader(http.StatusInternalServerError)
 	}
 
-	response.Write([]byte(err.Error()))
+	errDetail := struct {
+		Detail string `json:"detalhe"`
+	}{
+		Detail: err.Error(),
+	}
+
+	errMessage, err := json.Marshal(errDetail)
+	if err != nil {
+		slog.Error("unable to marshal err detail json", "err", err.Error())
+		return
+	}
+	response.Write(errMessage)
 }
